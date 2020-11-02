fd1d79c7cf60732caa73e36070451256954a6286
Aleksey Dolinin <allDolinin@gmail.com>
2020-11-02 21:45:47 +0300

    поправлен цвет иконок табара

--- RM Client/Controllers/TabViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class TabViewController: UITabBarController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("TBBAR")
-    }
-}
---
+++ RM Client/Controllers/TabbarViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class TabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.unselectedItemTintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.4035926871)
+        tabBarController?.tabBar.isTranslucent = false
+
+    }
+}
