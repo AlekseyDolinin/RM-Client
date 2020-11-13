e4e82a2c94eb62962f524f550ec5680798bb2079
Aleksey Dolinin <allDolinin@gmail.com>
2020-11-13 05:27:35 +0300

    переделано получение тасков

--- RM Client/Controllers/StartViewController.swift
+++ RM Client/Controllers/StartViewController.swift
@@ -62,81 +62,77 @@ class StartViewController: UIViewController, StoreSubscriber {
             }
         }
     }
-    
+
     // получение задач пользователя
     func getTasks(offset: Int) {
         self.startView.stateLabel.text = "Запрос задач пользователя"
         API.shared.getJSONPagination(
             endPoint: "/issues.json?assigned_to_id=\((mainStore.state.userData?.id)!)",
             offset: offset,
-            limit: 100,
+            limit: 10,
             completion: { (json) in
                 
                 let totalTasks = json["issues"].count
-                
                 var listAllTasks = [Task]()
+                var listCustomFields = [CustomField]()
+                var listAttachments = [Attachment]()
+                
+                print(json["issues"])
                 
                 for i in json["issues"] {
-                    let idTask = i.1["id"].intValue
+                    let dataTask = i.1
+                    for i in dataTask["custom_fields"] {
+                        let customFieldData = i.1
+                        let customField = CustomField(id: customFieldData["id"].intValue,
+                                                      name: customFieldData["name"].stringValue,
+                                                      value: customFieldData["value"].stringValue)
+                        listCustomFields.append(customField)
+                    }
                     
-                    API.shared.getDataTask(endPoint: "/issues/\(idTask)", completion: { (json) in
-                        let dataTask = json["issue"]
-                        var listCustomFields = [CustomField]()
-                        
-                        for i in dataTask["custom_fields"] {
-                            let customFieldData = i.1
-                            let customField = CustomField(id: customFieldData["id"].intValue,
-                                                          name: customFieldData["name"].stringValue,
-                                                          value: customFieldData["value"].stringValue)
-                            listCustomFields.append(customField)
-                        }
-                        var listAttachments = [Attachment]()
-                        
-                        for i in dataTask["attachments"] {
-                            let attachmentData = i.1
-                            let attachment = Attachment(id: attachmentData["id"].intValue,
-                                                        fileName: attachmentData["filename"].stringValue,
-                                                        createdOn: attachmentData["created_on"].stringValue,
-                                                        thumbnail_url: attachmentData["thumbnail_url"].stringValue,
-                                                        author: attachmentData["author"]["name"].stringValue,
-                                                        filesize: attachmentData["filesize"].intValue,
-                                                        description: attachmentData["description"].stringValue,
-                                                        content_type: attachmentData["content_type"].stringValue,
-                                                        content_url: attachmentData["content_url"].stringValue)
-                            listAttachments.append(attachment)
-                        }
-                        let task = Task(doneRatio: dataTask["done_ratio"].intValue,
-                                        startDate: dataTask["start_date"].stringValue,
-                                        createdOn: dataTask["created_on"].stringValue,
-                                        tracker: dataTask["tracker"]["name"].stringValue,
-                                        author: dataTask["author"]["name"].stringValue,
-                                        dueDate: dataTask["due_date"].stringValue,
-                                        subject: dataTask["subject"].stringValue,
-                                        isPrivate: dataTask["is_private"].boolValue,
-                                        assignedTo: dataTask["assigned_to"]["name"].stringValue,
-                                        category: dataTask["category"]["name"].stringValue,
-                                        estimatedHours: dataTask["estimated_hours"].doubleValue,
-                                        customFields: listCustomFields,
-                                        priority: dataTask["priority"]["name"].stringValue,
-                                        id: dataTask["id"].intValue,
-                                        parent: dataTask["parent"]["id"].intValue,
-                                        updatedOn: dataTask["updated_on"].stringValue,
-                                        closedOn: dataTask["closed_on"].boolValue,
-                                        status: dataTask["status"]["name"].stringValue,
-                                        description: dataTask["description"].stringValue,
-                                        project: dataTask["project"]["name"].stringValue,
-                                        attachments: listAttachments)
-                        
-                        listAllTasks.append(task)
-                        
+                    for i in dataTask["attachments"] {
+                        let attachmentData = i.1
+                        let attachment = Attachment(id: attachmentData["id"].intValue,
+                                                    fileName: attachmentData["filename"].stringValue,
+                                                    createdOn: attachmentData["created_on"].stringValue,
+                                                    thumbnail_url: attachmentData["thumbnail_url"].stringValue,
+                                                    author: attachmentData["author"]["name"].stringValue,
+                                                    filesize: attachmentData["filesize"].intValue,
+                                                    description: attachmentData["description"].stringValue,
+                                                    content_type: attachmentData["content_type"].stringValue,
+                                                    content_url: attachmentData["content_url"].stringValue)
+                        listAttachments.append(attachment)
+                    }
+                    
+                    let task = Task(doneRatio: dataTask["done_ratio"].intValue,
+                                    startDate: dataTask["start_date"].stringValue,
+                                    createdOn: dataTask["created_on"].stringValue,
+                                    tracker: dataTask["tracker"]["name"].stringValue,
+                                    author: dataTask["author"]["name"].stringValue,
+                                    dueDate: dataTask["due_date"].stringValue,
+                                    subject: dataTask["subject"].stringValue,
+                                    isPrivate: dataTask["is_private"].boolValue,
+                                    assignedTo: dataTask["assigned_to"]["name"].stringValue,
+                                    category: dataTask["category"]["name"].stringValue,
+                                    estimatedHours: dataTask["estimated_hours"].doubleValue,
+                                    customFields: listCustomFields,
+                                    priority: dataTask["priority"]["name"].stringValue,
+                                    id: dataTask["id"].intValue,
+                                    parent: dataTask["parent"]["id"].intValue,
+                                    updatedOn: dataTask["updated_on"].stringValue,
+                                    closedOn: dataTask["closed_on"].boolValue,
+                                    status: dataTask["status"]["name"].stringValue,
+                                    description: dataTask["description"].stringValue,
+                                    project: dataTask["project"]["name"].stringValue,
+                                    attachments: listAttachments)
+                    
+                    listAllTasks.append(task)
+                    mainStore.dispatch(LoadedUserTasks(userTasks: listAllTasks))
+                    
+                    // если скачались все задачи и статусы компании -> сортировка задач по статусам
+                    if totalTasks == mainStore.state.userTasks.count {
                         mainStore.dispatch(LoadedUserTasks(userTasks: listAllTasks))
-                        
-                        // если скачались все задачи и статусы компании -> сортировка задач по статусам
-                        if totalTasks == mainStore.state.userTasks.count {
-                            mainStore.dispatch(LoadedUserTasks(userTasks: listAllTasks))
-                            self.separateTasks()
-                        }
-                    })
+                        self.separateTasks()
+                    }
                 }
         })
     }
--- RM Client/Network/API.swift
+++ RM Client/Network/API.swift
@@ -17,8 +17,8 @@ class API {
         }
     }
     
-    func getJSONPagination(endPoint: String, offset: Int?, limit: Int?, completion: @escaping (JSON) -> Void) {
-        let request = "https://\(user):\(password)@\(path)\(endPoint).json?offset=\(offset ?? 0)&limit=\(limit ?? 0)"
+    func getJSONPagination(endPoint: String, offset: Int, limit: Int, completion: @escaping (JSON) -> Void) {
+        let request = "https://\(user):\(password)@\(path)\(endPoint)&include=attachments&offset=\(offset)&limit=\(limit)"
         Alamofire.request(request, method: .get).responseJSON { response in
             if response.result.isSuccess == false {
                 print("ERROR GET JSON Pagination")
@@ -45,20 +45,20 @@ class API {
         }
     }
     
-    func getDataTask(endPoint: String, completion: @escaping (JSON) -> Void) {
-        
-        let request = "https://\(user):\(password)@\(path)\(endPoint).json?include=attachments"
-        Alamofire.request(request, method: .get).responseJSON { response in
-            if response.result.isSuccess == false {
-                print("ERROR GET JSON")
-                return
-            }
-            if let data = response.data {
-                let json = JSON(data)
-                completion(json)
-            }
-        }
-    }
+//    func getDataTask(endPoint: String, completion: @escaping (JSON) -> Void) {
+//
+//        let request = "https://\(user):\(password)@\(path)\(endPoint).json?include=attachments"
+//        Alamofire.request(request, method: .get).responseJSON { response in
+//            if response.result.isSuccess == false {
+//                print("ERROR GET JSON")
+//                return
+//            }
+//            if let data = response.data {
+//                let json = JSON(data)
+//                completion(json)
+//            }
+//        }
+//    }
     
     func getAttachmentThumbnail(idAttachment: Int, completion: @escaping (UIImage) -> Void) {
         let request = "https://\(user):\(password)@\(path)/attachments/thumbnail/\(idAttachment)"
