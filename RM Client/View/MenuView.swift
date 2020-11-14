2cebcdcaa5de0d68b3bb64c9cfc7f05c7a4789f1
Aleksey Dolinin <allDolinin@gmail.com>
2020-11-14 20:44:18 +0300

    Добавлена иконка приложения
    верстка меню

--- RM Client/View/MenuView.swift
+++ RM Client/View/MenuView.swift
@@ -3,10 +3,18 @@ import UIKit
 class MenuView: UIView {
 
     @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var userFullName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
     
     func configure() {
-        print(mainStore.state.userData?.avatar)
+        
+        avatarImage.layer.cornerRadius = 4
         avatarImage.image = mainStore.state.userData?.avatar
+        
+        let lastname: String = mainStore.state.userData!.lastname!
+        let firstname: String = mainStore.state.userData!.firstname!
+        userFullName.text = "\(lastname) \(firstname)"
+        userEmail.text = mainStore.state.userData?.mail
     }
     
 }
