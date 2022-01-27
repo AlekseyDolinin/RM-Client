bfdcba3ba5d524f564c3a38c0622c7a0e878a740
allDolinin <allDolinin@gmail.com>
2022-01-27 17:10:34 +0300

    12

---
+++ RM Client/Controllers/Tasks/TableDelegates.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+extension RootTasksViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let taskCell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskCell
+        
+            
+        return taskCell
+    }
+    
+    
+    func tableView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Parse.listTasksStatuses.count
+    }
+
+}
---
+++ RM Client/Extentions/UITextField.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+extension UITextField {
+    func togglePasswordVisibility() {
+        isSecureTextEntry = !isSecureTextEntry
+        if let existingText = text, isSecureTextEntry {
+            deleteBackward()
+            
+            if let textRange = textRange(from: beginningOfDocument, to: endOfDocument) {
+                replace(textRange, withText: existingText)
+            }
+        }
+        if let existingSelectedTextRange = selectedTextRange {
+            selectedTextRange = nil
+            selectedTextRange = existingSelectedTextRange
+        }
+    }
+}
