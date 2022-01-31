2074e8a809ce8346c6e7cb621b06b98fce7bb5ee
allDolinin <allDolinin@gmail.com>
2022-01-31 11:00:12 +0300

    Открытие файлов

--- RM Client/Controllers/Attachment/AttachmentViewController.swift
+++ RM Client/Controllers/Attachment/AttachmentViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import WebKit
 
-class AttachmentViewController: UIViewController  {
+class AttachmentViewController: UIViewController, WKNavigationDelegate  {
 
     var viewSelf: AttachmentView! {
         guard isViewLoaded else {return nil}
@@ -12,6 +12,8 @@ class AttachmentViewController: UIViewController  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewSelf.webView.navigationDelegate = self
         
         let headers: [String: String]? = API.shared.apikey.isEmpty ? nil : ["X-Redmine-API-Key": API.shared.apikey]
         guard let url = URL(string: link) else {return}
--- RM Client/Controllers/Attachments/AttachmentsDelegates.swift
+++ RM Client/Controllers/Attachments/AttachmentsDelegates.swift
@@ -20,29 +20,9 @@ extension AttachmentsViewController: UITableViewDelegate, UITableViewDataSource 
     
     ///
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let vc = UIStoryboard(name: "Tasks", bundle: nil).instantiateViewController(withIdentifier: "PDFAttachmentVC") as! AttachmentViewController
+        let vc = UIStoryboard(name: "Tasks", bundle: nil).instantiateViewController(withIdentifier: "AttachmentViewController") as! AttachmentViewController
         vc.link = (self.task?.attachments[indexPath.row].contentURL)!
         self.navigationController?.pushViewController(vc, animated: true)
-        
-//        let headers: [String: String]? = API.shared.apikey.isEmpty ? nil : ["X-Redmine-API-Key": API.shared.apikey]
-//        let linkString = (self.task?.attachments[indexPath.row].contentURL)!
-//        guard let url = URL(string: linkString) else {return}
-        
-//        do {
-//            let req: URLRequest = try URLRequest(url: url, method: .get, headers: headers)
-//            print(req)
-//            let vc = SafariViewController(url: req.url!, configuration: SFSafariViewController.Configuration())
-//            present(vc, animated: true)
-//        } catch _ {
-//            print(12)
-//        }
-                
-//        do {
-//            _ = try wk.load(URLRequest(url: url, method: .get, headers: headers))
-//        } catch {
-//            print(error)
-//        }
     }
     
 }
